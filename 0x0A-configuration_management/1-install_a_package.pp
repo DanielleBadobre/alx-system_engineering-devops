7459f4e1ba259ac72b18c5e2f1d2fbb2175ca2b7
Danielle Badobre <badobredanielle@gmail.com>
2024-04-21 16:22:23 +0000

    try weur

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #install package
 
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
 exec { 'install flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/bin/',
