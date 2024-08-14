b72a16724984e1ad49bfb21e761620d3f458e3f2
Danielle Badobre <badobredanielle@gmail.com>
2024-08-14 15:40:44 +0000

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ show number of subs for subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     try:
-        response = request.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return ['data']['subscribers']
+            return data['data']['subscribers']
         else:
             return 0
     except requests.RequestException:
