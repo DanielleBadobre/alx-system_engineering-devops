c98e4575ee4d0f5a4264e10e3fbd550c2dfefe16
Danielle Badobre <badobredanielle@gmail.com>
2024-08-16 19:42:09 +0000

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
+"""Find the number of subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ show number of subs for subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
+    """ show the number of subscribers in a subreddit """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    # Change the user agent
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+        headers = {'User-Agent': 'cmmolanos'}
+        request = requests.get('https://api.reddit.com/r/{}/about'.
+                               format(subreddit),
+                               headers=headers)
+        subscribers = request.json()
+
+        sub = subscribers['data']['subscribers']
+        return sub
+    except:
         return 0
